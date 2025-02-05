4c0ce04e488e747f35013e7e362a7fb47db1a972
lviniciuszup <luiz.vincius@zup.com.br>
2025-02-05 15:51:03 -0300

    feat: add Client repository

--- ecommerce/src/main/java/com/zup/ecommerce/repository/ClientRepository.java
+++ ecommerce/src/main/java/com/zup/ecommerce/repository/ClientRepository.java
@@ -1,7 +1,12 @@
 package com.zup.ecommerce.repository;
 
-import com.zup.ecommerce.model.Product;
+import com.zup.ecommerce.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+import java.util.Optional;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+    Optional<Client> findByCpf(String cpf);
+
+    boolean existsByCpf(String cpf);
 }
