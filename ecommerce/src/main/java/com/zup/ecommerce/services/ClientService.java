3a66368cbcffa33497be6f16d83de372bf90c50a
lviniciuszup <luiz.vincius@zup.com.br>
2025-02-05 15:54:27 -0300

    feat: add ClientService

--- ecommerce/src/main/java/com/zup/ecommerce/services/ClientService.java
+++ ecommerce/src/main/java/com/zup/ecommerce/services/ClientService.java
@@ -1,37 +1,47 @@
 package com.zup.ecommerce.services;
 
-import com.zup.ecommerce.model.Product;
-import com.zup.ecommerce.repository.ProductRepository;
+import com.zup.ecommerce.model.Client;
+import com.zup.ecommerce.repository.ClientRepository;
+import jakarta.persistence.EntityExistsException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-
 @Service
-public class ProductService {
-    private final ProductRepository productRepository;
+public class ClientService {
+    private final ClientRepository clientRepository;
 
-    public ProductService(ProductRepository productRepository){
-        this.productRepository = productRepository;
+    public ClientService (ClientRepository clientRepository){
+        this.clientRepository = clientRepository;
     }
 
-    public Product saveProduct(Product product){
-       if (productRepository.existsByName(product.getName())){
-           throw new IllegalArgumentException("Já existe um produto com esse nome.");
-       }
-        return productRepository.save(product);
-    }
-    public List<Product> listAllProducts(){
-        return productRepository.findAll();
-    }
-
-    public void deleteProduct(Long id){
-        Optional<Product> existingProduct = productRepository.findById(id);
-        if(existingProduct.isEmpty()){
-            throw new IllegalArgumentException("O produto não existe");
+    public Client createClient(Client client){
+        if (clientRepository.existsByCpf(client.getCpf())){
+            throw new EntityExistsException ("Já existe um cliente com esse CPF!");
         }
-        productRepository.deleteById(id);
-
+        return clientRepository.save(client);
     }
+
+    public Client listClientByCpf(String cpf){
+        Optional <Client> existingClient = clientRepository.findByCpf(cpf);
+        if (existingClient.isEmpty()){
+            throw new IllegalArgumentException("Esse cliente não existe");
+        }
+        return existingClient.get();
+    }
+
+    public Client updateClient(Long id, Client client){
+        Optional <Client> existingClientById = clientRepository.findById(id);
+        if (existingClientById.isEmpty()){
+            throw new IllegalArgumentException("Esse cliente não existe");
+        }
+        Client existingClient = existingClientById.get();
+        existingClient.setName(client.getName());
+        existingClient.setEmail(client.getEmail());
+        existingClient.setCpf(client.getCpf());
+
+        return clientRepository.save(existingClient);
+    }
+
 }
