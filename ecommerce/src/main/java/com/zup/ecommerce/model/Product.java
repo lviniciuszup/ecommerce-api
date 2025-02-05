7a5b592503a53af19a6ae47e2ce931acff69982a
lviniciuszup <luiz.vincius@zup.com.br>
2025-02-05 15:59:10 -0300

    feat:  add Product entity

--- ecommerce/src/main/java/com/zup/ecommerce/model/Product.java
+++ ecommerce/src/main/java/com/zup/ecommerce/model/Product.java
@@ -1,4 +1,67 @@
 package com.zup.ecommerce.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    @NotBlank(message = "O nome não pode ficar vázio!")
+    private String name;
+    @Positive(message = "O preço deve ser maior que 0")
+    private Double price;
+    @Min(value = 0, message = "A quantidade não pode ser negativa")
+    private Integer quantity;
+
+    public Product(){
+
+    }
+
+    public Product(Long id, String name, Double price, Integer quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
