2cffec670b65ef85daae65610cb708de8583bd7c
lviniciuszup <luiz.vincius@zup.com.br>
2025-02-05 16:01:59 -0300

    feat: add ProductController

--- ecommerce/src/main/java/com/zup/ecommerce/controller/ProductController.java
+++ ecommerce/src/main/java/com/zup/ecommerce/controller/ProductController.java
@@ -1,4 +1,36 @@
 package com.zup.ecommerce.controller;
 
+import com.zup.ecommerce.model.Product;
+import com.zup.ecommerce.services.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
 public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService){
+        this.productService = productService;
+    }
+    @PostMapping("/products")
+    public ResponseEntity<Product> newProduct(@RequestBody Product product){
+        Product savedProduct = productService.saveProduct(product);
+        return ResponseEntity.status(201).body(savedProduct);
+    }
+    @GetMapping("/products")
+    public List<Product> listProducts(){
+        return productService.listAllProducts();
+    }
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Void> deleteProductbyId(@PathVariable Long id){
+        try {
+            productService.deleteProduct(id);
+            return ResponseEntity.noContent().build();
+        } catch (IllegalArgumentException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
