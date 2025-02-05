7066cc0a286d1fe3ee3a18fdeac686577f817e81
lviniciuszup <luiz.vincius@zup.com.br>
2025-02-05 16:04:44 -0300

    feat: add Order entity and OrderStatus enum

--- ecommerce/src/main/java/com/zup/ecommerce/model/Order.java
+++ ecommerce/src/main/java/com/zup/ecommerce/model/Order.java
@@ -1,4 +1,82 @@
 package com.zup.ecommerce.model;
 
-public class Pedidos {
+import jakarta.persistence.*;
+
+import java.util.Calendar;
+import java.util.List;
+
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne // Muitos objetos dessa entitdade para um unico cliente
+    private Client client;
+    @ManyToMany
+    @JoinTable(
+            name = "product_order",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private List<Product> products;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Calendar dateHour;
+    private Double totalValue;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public Calendar getDateHour() {
+        return dateHour;
+    }
+
+    public void setDateHour(Calendar dateHour) {
+        this.dateHour = dateHour;
+    }
+
+    public Double getTotalValue() {
+        return totalValue;
+    }
+
+    public void setTotalValue(Double totalValue) {
+        this.totalValue = totalValue;
+    }
+
+    public Order(Client client, List<Product> products, OrderStatus orderStatus, Calendar dateHour) {
+        this.client = client;
+        this.products = products;
+        this.orderStatus = orderStatus;
+        this.dateHour = dateHour;
+    }
 }
--- ecommerce/src/main/java/com/zup/ecommerce/model/OrderStatus.java
+++ ecommerce/src/main/java/com/zup/ecommerce/model/OrderStatus.java
@@ -1,4 +1,5 @@
 package com.zup.ecommerce.model;
 
 public enum OrderStatus {
+    PROCESSING, PAID, DENEID
 }
