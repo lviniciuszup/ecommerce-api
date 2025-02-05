c1916816a5489adebb00c1bd6f9a24104d86c5fd
lviniciuszup <luiz.vincius@zup.com.br>
2025-02-05 15:59:59 -0300

    feat: add Product repository

--- ecommerce/src/main/java/com/zup/ecommerce/repository/ProductRepository.java
+++ ecommerce/src/main/java/com/zup/ecommerce/repository/ProductRepository.java
@@ -1,4 +1,18 @@
 package com.zup.ecommerce.repository;
 
-public interface ProductRepository {
+import com.zup.ecommerce.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+import static ch.qos.logback.core.joran.spi.ConsoleTarget.findByName;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    //Metodo que busca um produto pelo nome, pode retornar um produto ou nao.
+    Optional<Product> findByName(String name);
+
+    //Verifica se o metodo tem algum retorno, isPresent informa se existe
+    default boolean existsByName(String name){
+        return findByName(name).isPresent();
+    }
 }
