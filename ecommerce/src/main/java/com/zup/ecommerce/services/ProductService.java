ec88b8325ae43af9b62d7ad98f8abef0d546d4ea
lviniciuszup <luiz.vincius@zup.com.br>
2025-02-05 16:01:12 -0300

    feat: add ProductService

--- ecommerce/src/main/java/com/zup/ecommerce/services/ProductService.java
+++ ecommerce/src/main/java/com/zup/ecommerce/services/ProductService.java
@@ -1,4 +1,37 @@
 package com.zup.ecommerce.services;
 
+import com.zup.ecommerce.model.Product;
+import com.zup.ecommerce.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
 public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+    public Product saveProduct(Product product){
+       if (productRepository.existsByName(product.getName())){
+           throw new IllegalArgumentException("Já existe um produto com esse nome.");
+       }
+        return productRepository.save(product);
+    }
+    public List<Product> listAllProducts(){
+        return productRepository.findAll();
+    }
+
+    public void deleteProduct(Long id){
+        Optional<Product> existingProduct = productRepository.findById(id);
+        if(existingProduct.isEmpty()){
+            throw new IllegalArgumentException("O produto não existe");
+        }
+        productRepository.deleteById(id);
+
+    }
 }
