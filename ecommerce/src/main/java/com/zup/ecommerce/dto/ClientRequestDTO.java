7711543b862eb12f8b9fea518c48472cd8699cab
lviniciuszup <luiz.vincius@zup.com.br>
2025-02-18 14:57:56 -0300

    feat: create ClientRequesDTO for Client

--- ecommerce/src/main/java/com/zup/ecommerce/dto/ClientRequestDTO.java
+++ ecommerce/src/main/java/com/zup/ecommerce/dto/ClientRequestDTO.java
@@ -2,11 +2,45 @@ package com.zup.ecommerce.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.br.CPF;
 
-public record ClientDTO(
-    @NotBlank(message = "O nome não pode ser vázio") String name,
-    @NotBlank(message = "O cpf não pode ser vázio") String cpf,
-    @Email(message = "Insira um email valido por favor!") String email
-) {
+public class ClientRequestDTO {
+    @NotBlank(message = "O nome do cliente não pode ser vazio.")
+    private String name;
+    @NotBlank(message = "O CPF não pode ser vazio")
+    @CPF(message = "O cpf deve ser válido!")
+    private String cpf;
+    @Email(message = "O e-mail não pode ser inválido")
+    @NotBlank(message = "O e-mail não pode ser vázio")
+    private String email;
 
+    public ClientRequestDTO(String name, String cpf, String email) {
+        this.name = name;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
